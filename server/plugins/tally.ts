365c9263e5f970e12f1f4701c88271d0981912ca
Solomon Eseme <kaperskyguru@gmail.com>
2024-04-17 01:54:17 +0100

    fix: add script

--- server/plugins/tally.ts
+++ server/plugins/tally.ts
@@ -1,11 +1,11 @@
 export default defineNitroPlugin((nitroApp: any) => {
-  //   if (process.env.NODE_ENV == "development") return;
+  if (process.env.NODE_ENV == "development") return;
   nitroApp.hooks.hook("render:html", (html: any, { event }: any) => {
     html.head.push(`
     
       <script async src="https://tally.so/widgets/embed.js"></script>
 
-      <script defer data-domain="boilerplatesearch.netlify.app" src="https://plausible.io/js/script.js"></script>
+      <script defer data-domain="boilerplatesearch.com" src="https://plausible.io/js/script.js"></script>
 
       `);
   });
