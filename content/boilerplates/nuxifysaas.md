1d9226268abd8a793a1056d676b786fe9a408923
Solomon Eseme <kaperskyguru@gmail.com>
2024-05-06 12:46:48 +0100

    fix: add NuxifySaaS

---
+++ content/boilerplates/nuxifysaas.md
@@ -0,0 +1,30 @@
+---
+title: "NuxifySaaS"
+description: "Nuxt SaaS Boilerplate with ready-to-go components for Payments, Auth, Admin, Blog, SEO, Database, Mailing, Templating system(Landing pages, Dashboards) and everything you need to ship your SaaS in days!."
+price: "$99.99 - $499.99"
+tags:
+  [
+    "Nuxt.js",
+    "Authentication",
+    "Payments",
+    "Social Auth",
+    "Admin",
+    "SEO",
+    "Tailwind CSS",
+    "SEO",
+    "pSEO",
+    "Landing Pages",
+    "Blog",
+    "Customizable UI",
+    "Storage providers",
+    "Databases",
+    "Analytics",
+    "Dashboards",
+  ]
+isPopular: true
+isTrending: true
+shouldWriteReview: true
+isFeatured: true
+url: https://nuxifysaas.com
+image: nuxifysaas.png
+---
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,60 @@
 <template>
-  <main class="bg-black w-full text-white">
-    <div class="flex flex-col items-center justify-center pt-10">
-      <div class="flex items-center">
-        <!-- <div class="h-40 w-40">
+  <span>
+    <div
+      style="background: #0d7261; padding: 20px; display: flex"
+      class="fixed z-20 w-full pb-10 justify-center"
+    >
+      <h2 style="font-size: 1.3rem; font-weight: bold; color: #fff">
+        Pre-order my new Nuxt SaaS Boilerplate at 50% discount.
+        <a
+          class="underline text-gray-300;"
+          target="_blank"
+          href="https://nuxifysaas.com?utm_source=BoilerplateSearch"
+          >Click here for more details</a
+        >
+      </h2>
+    </div>
+    <main class="bg-black w-full text-white md:pt-20 pt-36">
+      <div class="flex flex-col items-center justify-center pt-10">
+        <div class="flex items-center">
+          <!-- <div class="h-40 w-40">
           <img src="/img/logo.png" class="w-full h-full" alt="" />
         </div> -->
-        <h1
-          class="max-w-4xl text-center text-5xl font-bold md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"
-        >
-          <a href="/"> Boilerplate Search </a>
-        </h1>
+          <h1
+            class="max-w-4xl text-center text-5xl font-bold md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"
+          >
+            <a href="/"> Boilerplate Search </a>
+          </h1>
+        </div>
+        <p class="text-light-100 mt-5 max-w-xl text-center">
+          A one-stop-shop for your boilerplate search for your next project.
+        </p>
+        <div class="flex items-center gap-1">
+          <a
+            class="py-3 font-bold"
+            href="/blog/why-you-need-a-boilerplate-for-your-next-saas-project"
+            >Why use boilerplates?
+          </a>
+          |
+          <a class="py-3 font-bold" href="/blog"> Visit our blog</a>
+          | <a class="py-3 font-bold" href="/tops">Our Top Lists</a>
+        </div>
       </div>
-      <p class="text-light-100 mt-5 max-w-xl text-center">
-        A one-stop-shop for your boilerplate search for your next project.
-      </p>
-      <div class="flex items-center gap-1">
-        <a
-          class="py-3 font-bold"
-          href="/blog/why-you-need-a-boilerplate-for-your-next-saas-project"
-          >Why use boilerplates?
-        </a>
-        |
-        <a class="py-3 font-bold" href="/blog"> Visit our blog</a>
-        | <a class="py-3 font-bold" href="/tops">Our Top Lists</a>
-      </div>
-    </div>
 
-    <slot />
+      <slot />
 
-    <footer class="py-20 flex justify-center">
-      <p>
-        Built and curated by
-        <a
-          class="underline text-gray-300"
-          href="https://twitter.com/kaperskyguru"
-          >Solomon Eseme</a
-        >.
-      </p>
-    </footer>
-  </main>
+      <footer class="py-20 flex justify-center">
+        <p>
+          Built and curated by
+          <a
+            class="underline text-gray-300"
+            href="https://twitter.com/kaperskyguru"
+            >Solomon Eseme</a
+          >.
+        </p>
+      </footer>
+    </main>
+  </span>
 </template>
 
 <script setup>
